6a495019fc5f8c25385a062079a42076827135df
zenodante <yeming@outlook.com>
2025-04-04 10:01:28 -0500

    Add files via upload

---
+++ picocalcdisplay.h
@@ -0,0 +1,22 @@
+#ifndef _PICOCALCDISPLAY_H
+#define _PICOCALCDISPLAY_H
+
+#define DISPLAY_WIDTH 320
+#define DISPLAY_HEIGHT 320
+
+                //cs;sck;mosi;miso;dc;bl;
+                //13 10 11 12 14 0
+
+
+#define CLK_PIN 10
+#define MOSI_PIN 11
+#define CS_PIN  13
+#define DC_PIN 14
+#define RST_PIN 15
+#define SPI_DISP spi1
+
+
+
+
+
+#endif // ILI9488_H
