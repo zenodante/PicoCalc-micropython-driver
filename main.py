428912e208dbeb6bc8a5fb4e1e08b05bec248bde
Ming <yeming@outlook.com>
2025-04-04 21:48:34 -0700

    modified main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from picocalc import PicoDisplay, PicoKeyboard
 from fbconsole import FBConsole
+import os
 pd = PicoDisplay(320,320)
 kb = PicoKeyboard()
-FBConsole( pd, bgcolor=0, fgcolor=2, width=320, height=320,readobj=kb,fontX=6,fontY=8)
+fb=FBConsole( pd, bgcolor=0, fgcolor=2, width=320, height=320,readobj=kb,fontX=6,fontY=8)
+os.dupterm(fb) 
