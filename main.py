c8b63ac60578ca9f145f29c0fd9f767327ad20bc
LaikaDawg <jsrusso20@gmail.com>
2025-04-08 16:27:08 -0400

    added sd card automounting on boot
    automatically mounts sd card to /sd upon boot using initsd(), use killsd() if you need to unmount

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from picocalc import PicoDisplay, PicoKeyboard
 from fbconsole import FBConsole
+import uos
 import os
+
+# Separated imports because Micropython is super finnicky
+from picocalc_system import initsd as initsd
+from picocalc_system import killsd as killsd
+
+# Mount SD card to /sd on boot
+sd = initsd()
+
 pd = PicoDisplay(320,320)
 kb = PicoKeyboard()
 fb=FBConsole( pd, bgcolor=0, fgcolor=2, width=320, height=320,readobj=kb,fontX=6,fontY=8)
