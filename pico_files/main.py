db8a4717db6b983637a462db3e35a7f42355e347
Ming <yeming@outlook.com>
2025-04-15 16:38:00 -0700

    reg the display,keyboard and terminal to global

--- pico_files/main.py
+++ pico_files/main.py
@@ -1,8 +1,8 @@
 from picocalc import PicoDisplay, PicoKeyboard
 from fbconsole import FBConsole
-import uos
 import os
 import vt
+import builtins
 
 # Separated imports because Micropython is super finnicky
 from picocalc_system import initsd as initsd
@@ -10,10 +10,12 @@ from picocalc_system import killsd as killsd
 
 # Mount SD card to /sd on boot
 
-pd = PicoDisplay(320,320)#display
-kb = PicoKeyboard()#keyboard
-fb=FBConsole( pd, bgcolor=0, fgcolor=2, width=320, height=320,readobj=kb,fontX=6,fontY=8)#fbconsole
-te = vt.vt(pd,kb)#terminal
+builtins.pc_display = PicoDisplay(320,320)#display
+builtins.pc_keyboard = PicoKeyboard()#keyboard
+fb=FBConsole( builtins.pc_display , bgcolor=0, fgcolor=2, width=320, height=320,readobj=builtins.pc_keyboard ,fontX=6,fontY=8)#fbconsole
+builtins.vt_terminal = vt.vt(builtins.pc_display,builtins.pc_keyboard)#terminal
+builtins.pc_sd = initsd()#sd card
 os.dupterm(fb) 
 
-sd = initsd()#sd card
+
+#we may move all the init to boot.py later, but for now we want to keep it in main.py for testing purposes
