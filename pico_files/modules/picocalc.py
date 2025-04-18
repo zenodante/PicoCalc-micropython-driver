6f8e2b34e02b9747116fd20a89d93470f88bcb32
Ming <yeming@outlook.com>
2025-04-18 16:37:35 -0700

    manual show could be on both core

--- pico_files/modules/picocalc.py
+++ pico_files/modules/picocalc.py
@@ -59,8 +59,8 @@ class PicoDisplay(framebuf.FrameBuffer):
     def text(self,c, x0, y0, color):
         picocalcdisplay.drawTxt6x8(c,x0,y0,color)
 
-    def show(self):
-        picocalcdisplay.update()
+    def show(self,core=0):
+        picocalcdisplay.update(core)
 
 
 
